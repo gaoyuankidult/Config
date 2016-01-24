d2d009ac57542d426daef0e95bd5a2ec01340008
Yuan Gao <gaoyuankidult@gmail.com>
2016-01-24 08:12:04 +0200

    updated setup.sh for adding key for git

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-#!bin/bash
+#!/bin/bash
 
-change XKBOPTIONS="ctrl:nocaps" in /etc/default/keyboard
-then run sudo dpkg-reconfigure -phigh console-setup
+#change XKBOPTIONS="ctrl:nocaps" in /etc/default/keyboard
+# then run sudo dpkg-reconfigure -phigh console-setup
+
+ssh-keygen -t rsa -b 4096 -C "gaoyuankidult@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub
