bad36282cca328b37dc1c91ab0012530074ba5d2
Yuan Gao <gaoyuankidult@gmail.com>
2016-04-29 21:12:19 +0300

    change customization from .bash_profile to .profile

--- reset.sh
+++ reset.sh
@@ -41,7 +41,7 @@ if [[ $UID != 0 ]]; then
 fi
 
 # remove symbolic link to .emacs .gitconfig .gitignore
-rm ~/.bash_profile
+rm ~/.profile
 rm ~/.emacs.d/init.el
 rm ~/.emacs.d/load-directory-mu.el
 rm ~/.emacs.d/00-editor.el
